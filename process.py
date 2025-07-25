ed3b0406cb039203547a157fd8618408f8bfa1d5
Ana Brendel <anabrendel25@gmail.com>
2025-07-25 09:57:29 -0400

    update script and axioms to print results

---
+++ process.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+# success = 0
+success = "{\"status\": \"SUCCESS\""
+# fail = 1 
+fail = "{\"status\": \"FAILURE\""
+# incomplete = 2
+incomplete = "{\"status\": \"INCOMPLETE\""
+
+def str_status(stat):
+    if stat == 2:
+        return "INCOMPLETE"
+    elif stat == 1:
+        return "FAILURE"
+    elif stat == 0:
+        return "SUCCESS"
+    else:
+        return "ERROR"
+
+without = "search-report-without"
+without_fixed = "search-report-without-fixed"
+withs = "search-report-with-"
+axioms = "search-report-with-axioms-"
+
+def read_results(full):
+    read = lambda x: open(x, "r").read().split("\n")
+    for file in os.listdir(full):
+        if file.endswith("-proof.txt"):
+            contents = read(os.path.join(full,file))
+            for line in contents:
+                if line.__contains__(incomplete):
+                    return 2
+                elif line.__contains__(fail):
+                    return 1
+                elif line.__contains__(success):
+                    return 0
+    return -1
+            
+
+def driver(test):
+    r = {}
+    for elem in os.listdir(test):
+        if elem.startswith(without) or elem.startswith(without_fixed) or elem.startswith(withs) or elem.startswith(axioms):
+            full = os.path.join(test,elem)
+            r[elem] = str_status(read_results(full))
+
+
+full_test = sys.argv[1]
+driver(full_test)
--- script.py
+++ script.py
@@ -7,6 +7,13 @@ def cmd(test,axiom):
     c = f"python3 /home/proverbot/src/search_file.py --prelude={folder} --weightsfile=/home/proverbot/data/polyarg-weights.dat {folder}/source.v {a} --no-generate-report --max-proof-time=15 -P -o {folder}/search-report-{label}"
     return c
 
+def axiom_command(test,axiom_label):
+    folder = f"/home/proverbot/prover-tests/{test}"
+    a = f"--add-axioms={folder}/axioms.txt"
+    label = f"axioms-{axiom_label}"
+    c = f"python3 /home/proverbot/src/search_file.py --prelude={folder} --weightsfile=/home/proverbot/data/polyarg-weights.dat {folder}/{axiom_label}.v {a} --no-generate-report --max-proof-time=15 -P -o {folder}/search-report-{label}"
+    return c
+
 test_results = {
     "select_rest_length_by_select_perm" : [1,2,3],
     "selsort_sorted_by_select_rest_length_1" : [1,2,3,4,5],
@@ -27,7 +34,7 @@ test_results = {
     "le_all__le_one_by_Forall_forall" : [1,2,3,4,5],
     "cons_of_small_maintains_sort_ind_list_by_Permutation_in" : [1,2,3,4,5],
     "cons_of_small_maintains_sort_ind_list_by_le_all__le_one" : [1,2,3,4,5],
-    "select_rest_length_by_Permutation_length" : [1,2],
+    "select_rest_length_by_Permutation_length" : [1],
     "cons_of_small_maintains_sort_ind_length_by_select_rest_length" : [1,2,3],
     "select_in_by_Permutation_in" : [1,2],
     "selection_sort_sorted_by_selsort_sorted" : [1,2,3,4,5],
@@ -35,10 +42,15 @@ test_results = {
 }
 
 # for test in test_results:
-# for test in ["select_rest_length_by_select_perm"]:
-# for test in ["select_rest_length_by_Permutation_length"]:
-for test in ["selsort_sorted_by_select_rest_length_1"]:
+for test in ["select_rest_length_by_select_perm","select_rest_length_by_Permutation_length", "selsort_sorted_by_select_rest_length_1"]:
     os.system(cmd(test,-1))
     os.system(cmd(test,0))
     for i in test_results[test]:
         os.system(cmd(test,i))
+    full = f"/home/proverbot/prover-tests/{test}"
+    results = os.path.join(full,"result_summary")
+    for file in os.listdir(full):
+        if file.endswith(".v") and file.startswith("axioms"):
+            label = file.removesuffix(".v")
+            os.system(axiom_command(test,label))
+    os.system(f"python3 process.py {full} > {results}")
